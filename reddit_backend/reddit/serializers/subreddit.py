ed079c1d0448d5bef32518ae8d86c5050dc09de0
mostafa <mos1380n@gmail.com>
2022-01-26 00:12:34 +0330

    Update subreddit serializer

--- reddit_backend/reddit/serializers/subreddit.py
+++ reddit_backend/reddit/serializers/subreddit.py
@@ -3,7 +3,12 @@ from reddit.models import Subreddit
 
 
 class SubredditSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+
+    def create(self, validated_data):
+        super(SubredditSerializer, self).create({**validated_data, 'owner': self.context['request'].user})
 
     class Meta:
         model = Subreddit
         fields = '__all__'
+        read_only_fields = ['owner']
