da6d59434e5b5f538475d2b9a3ccecc50e2588f6
mostafa <mos1380n@gmail.com>
2022-01-26 12:08:35 +0330

    Update subreddit view and serilaizer

--- reddit_backend/reddit/views/subreddit.py
+++ reddit_backend/reddit/views/subreddit.py
@@ -1,7 +1,4 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import mixins, ModelViewSet
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.viewsets import ModelViewSet
 
 from reddit.serializers.subreddit import SubredditSerializer
 from reddit.models import Subreddit
