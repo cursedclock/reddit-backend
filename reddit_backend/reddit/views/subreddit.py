869adbd67f9479ff7343e11c7837aa1da2830e7a
mostafa <mos1380n@gmail.com>
2022-01-26 12:13:05 +0330

    Add subreddit permission base

--- reddit_backend/reddit/views/subreddit.py
+++ reddit_backend/reddit/views/subreddit.py
@@ -1,10 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from reddit.utils.permissions import SubredditPermissions
 from reddit.serializers.subreddit import SubredditSerializer
 from reddit.models import Subreddit
 
 
 class SubredditViewSet(ModelViewSet):
-
+    permission_classes = (IsAuthenticatedOrReadOnly, SubredditPermissions)
     queryset = Subreddit.objects.all()
     serializer_class = SubredditSerializer
