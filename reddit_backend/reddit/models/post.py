7070a55ec6b48d0ef7b4a5c7c2335cbf47e7b063
mostafa <mos1380n@gmail.com>
2022-01-26 19:48:42 +0330

    Add upvote and downvote to post

--- reddit_backend/reddit/models/post.py
+++ reddit_backend/reddit/models/post.py
@@ -11,7 +11,23 @@ class Post(models.Model):
     author = models.ForeignKey(to=User, related_name='posts', null=True, on_delete=models.SET_NULL)
     subreddit = models.ForeignKey(to=Subreddit, related_name='posts', on_delete=models.CASCADE)
     publish_date = models.DateTimeField(auto_now_add=True)
-    upvotes = models.IntegerField(default=0)
-    downvotes = models.IntegerField(default=0)
+
+    @property
+    def upvotes(self):
+        return PostVote.objects.filter(is_upvote=True, post=self).count()
+
+    @property
+    def downvotes(self):
+        return PostVote.objects.filter(is_upvote=False, post=self).count()
+
     # TODO comments
 
+
+class PostVote(models.Model):
+
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    is_upvote = models.BooleanField()
+
+    class Meta:
+        unique_together = [['post', 'user']]
--- reddit_backend/reddit/views/post.py
+++ reddit_backend/reddit/views/post.py
@@ -1,10 +1,10 @@
-from requests import Response
-from rest_framework.viewsets import ModelViewSet, mixins, GenericViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import mixins, GenericViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import action
 
 from reddit.utils.permissions import PostPermissions
-from reddit.serializers.post import PostSerializer
+from reddit.serializers.post import PostSerializer, PostVoteSerializer
 from reddit.models import Post
 
 
@@ -16,3 +16,26 @@ class PostViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin
     def get_serializer_class(self):
         if self.action in ['create', 'list', 'retrieve']:
             return PostSerializer
+        if self.action in ['upvote', 'downvote']:
+            return PostVoteSerializer
+
+    def get_serializer_context(self):
+        context = super(PostViewSet, self).get_serializer_context()
+        if self.action in ['upvote', 'downvote']:
+            context['post'] = self.get_object()
+            context['action'] = self.action
+        return context
+
+    @action(detail=True, methods=['PUT'])
+    def upvote(self, request, pk):
+        return self._vote(request, pk)
+
+    @action(detail=True, methods=['PUT'])
+    def downvote(self, request, pk):
+        return self._vote(request, pk)
+
+    def _vote(self, request, pk):
+        serializer = self.get_serializer(data={})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
