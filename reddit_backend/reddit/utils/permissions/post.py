ebc1d06bac0edaacc9ad63579681607efdc24871
mostafa <mos1380n@gmail.com>
2022-01-29 02:15:58 +0330

    Implement post permissions

--- reddit_backend/reddit/utils/permissions/post.py
+++ reddit_backend/reddit/utils/permissions/post.py
@@ -1,5 +1,14 @@
 from rest_framework.permissions import BasePermission
+from reddit.models import Membership
 
 
 class PostPermissions(BasePermission):
-    pass  # TODO implement post permissions
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            user = request.user
+            subreddit = request.body.get('subreddit')
+            if not subreddit:
+                return False
+            return Membership.objects.filter(user=user, subreddit=subreddit).exists()
+        else:
+            return True
