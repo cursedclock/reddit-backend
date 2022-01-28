5751c2473a7ec1ca8397be582b4d2b2f76a1ec2e
mostafa <mos1380n@gmail.com>
2022-01-28 22:49:54 +0330

    Add comment permissions template

--- reddit_backend/reddit/views/comment.py
+++ reddit_backend/reddit/views/comment.py
@@ -8,11 +8,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from reddit.serializers.comment import PostCommentSerializer, CommentReplySerializer, CommentVoteSerializer
 from reddit.models import PostComment
+from reddit.utils.permissions import CommentPermissions
 
 
 class CommentViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
 
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, CommentPermissions)
     queryset = PostComment.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     search_fields = ['text', 'on_post']
